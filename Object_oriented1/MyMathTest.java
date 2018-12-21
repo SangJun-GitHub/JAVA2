b3bb744a08f19cb39006dee909adfc432998d29c
SangJun-HitHub <sjpark24@myseneca.ca>
2018-12-20 19:25:44 -0500

    commit

---
+++ Object_oriented1/MyMathTest.java
@@ -0,0 +1,36 @@
+package Object_oriented1;
+
+/**
+ * Created by bobsang89@gmail.com on 2018-12-19
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+
+class MyMath {
+    long add(long a, long b) {
+        return a +b;
+    }
+    long subtract(long a, long b){
+        return a - b;
+    }
+    long multiply(long a, long b){
+        return a * b;
+    }
+    double divide(double a, double b){
+        return a / b;
+    }
+}
+public class MyMathTest {
+    public static void main(String[] args) {
+        MyMath mm = new MyMath();
+        long result1 = mm.add(5L, 3L);
+        long result2 = mm.subtract(5L, 3L);
+        long result3 = mm.multiply(5L, 3L);
+        double result4 = mm.divide(5L, 3L);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
