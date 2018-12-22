f13e57a95957ea01fec43a25b07daeac399a2cef
SangJun-HitHub <sjpark24@myseneca.ca>
2018-12-21 23:22:24 -0500

    commit

--- Object_oriented1/ReferenceReturnEx.java
+++ Object_oriented1/ReferenceReturnEx.java
@@ -10,14 +10,14 @@ public class ReferenceReturnEx {
         Data d = new Data();
         d.x = 10;
 
-        Data d2 = copy(d);
+        Data d2 = copy(d);      //d2 must be Data type because copy() return Data type
         System.out.println("d.x = " + d.x);
         System.out.println("d2.x = " + d2.x);
 
     }
     static Data copy(Data d){
-        Data tmp = new Data();
-        tmp.x = d.x;
+        Data tmp = new Data();  //Create new Data
+        tmp.x = d.x;            //Copy value from d.x to tmp.x
         return tmp;
     }
 }
