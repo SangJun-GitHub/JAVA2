99f8d4dbd304f406e2a0049e939006627360824d
SangJun-HitHub <sjpark24@myseneca.ca>
2018-12-24 21:35:27 -0500

    commit

---
+++ Object_oriented1/ConstructorTest.java
@@ -0,0 +1,21 @@
+package Object_oriented1;
+
+/**
+ * Created by bobsang89@gmail.com on 2018-12-24
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+class Data1{
+    int value;
+}
+class Data2{
+    int value;
+    Data2(int x){ value = x; }
+}
+public class ConstructorTest {
+    public static void main(String[] args) {
+        Data1 d1 = new Data1();
+    //    Data2 d2 = new Data2(); //error. Data2 constructor need argument
+        Data2 d2 = new Data2(d1.value);
+    }
+}
