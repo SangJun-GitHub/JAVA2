0e6aeb8ea63381c153d29b624e9ff241981b0937
SangJun-HitHub <sjpark24@myseneca.ca>
2019-01-06 23:32:34 -0500

    commit

---
+++ Exception_handling/ExceptionEx1.java
@@ -0,0 +1,9 @@
+package Exception_handling;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-01-06
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class ExceptionEx1 {
+}
