1472283a7aca62219748d0259cfaf214e26138df
SangJun-HitHub <sjpark24@myseneca.ca>
2019-01-09 23:21:36 -0500

    commit

---
+++ Exception_handling/ExceptionEx8.java
@@ -0,0 +1,13 @@
+package Exception_handling;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-01-09
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class ExceptionEx8 {
+    public static void main(String[] args) {
+//        throw new Exception();          //Intentional occurrence.
+                                        //compile error. There is no exception handling in the area where exception handling should be done
+    }
+}
