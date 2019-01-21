63f3dfa048356f731c5a878f0d5dc1f1c27bc8f1
SangJun-HitHub <sjpark24@myseneca.ca>
2019-01-21 15:31:01 -0500

    commit

---
+++ Package_Utill/WrapperEx1.java
@@ -0,0 +1,24 @@
+package Package_Utill;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-01-21
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class WrapperEx1 {
+    public static void main(String[] args) {
+        Integer i = new Integer(100);
+        Integer i2 = new Integer(100);
+
+        System.out.println("i == i2 ? " + (i == i2));
+        System.out.println("i.equals(i2) ? " + i.equals(i2));
+        System.out.println("i.compareTo(i2) = " + i.compareTo(i2));
+        System.out.println("i.toString() = " + i.toString());
+
+        System.out.println("MAX_VALUE = " + Integer.MAX_VALUE);
+        System.out.println("MIN_VALUE = " + Integer.MIN_VALUE);
+        System.out.println("SIZE = " + Integer.SIZE + "bits");
+        System.out.println("BYTES = " + Integer.BYTES + "bytes");
+        System.out.println("TYPE = " + Integer.TYPE);
+    }
+}
