169901779388595473754f3c2733c87385931284
SangJun-HitHub <sjpark24@myseneca.ca>
2019-01-22 22:49:33 -0500

    commit

---
+++ Package_Utill/RandomEx3.java
@@ -0,0 +1,36 @@
+package Package_Utill;
+
+import java.util.Arrays;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-01-22
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class RandomEx3 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++)
+            System.out.print(getRand(5, 10)+", ");
+        System.out.println();
+
+        int[] result = fillRand(new int[10], new int[]{2, 3, 7, 5});
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int getRand(int from, int to){
+        return (int)(Math.random() * (Math.abs(to-from) + 1 )) + Math.min(from, to);
+    }
+
+//    public static int[] fillRand(int[] arr, int from, int to){
+//        for(int i = 0; i < arr.length; i++)
+//            arr[i] = getRand(from, to);
+//        return arr;
+//    }
+
+    public static int[] fillRand(int[]arr, int[] data){
+        for(int i = 0; i < arr.length; i++)
+            arr[i] = data[getRand(0, data.length -1)];
+        return arr;
+    }
+}
