5e0b495715cc4841d23545e177d3248dcad74dc0
SangJun-HitHub <sjpark24@myseneca.ca>
2019-02-21 23:11:15 -0500

    commit

---
+++ Collection_framework/AsciiPrint.java
@@ -0,0 +1,14 @@
+package Collection_framework;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-02-21
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class AsciiPrint {
+    public static void main(String[] args) {
+        char ch = ' ';
+        for(int i = 0; i < 95; i++)
+            System.out.print(ch++);
+    }
+}
