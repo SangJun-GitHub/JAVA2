69bfba21cb7e84b30a67ecba67e02eba955a716d
SangJun-HitHub <sjpark24@myseneca.ca>
2019-02-24 23:23:51 -0500

    commit

---
+++ Collection_framework/PropertiesEx4.java
@@ -0,0 +1,14 @@
+package Collection_framework;
+
+import java.util.Properties;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-02-24
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class PropertiesEx4 {
+    public static void main(String[] args) {
+        Properties sysProp = System.getProperties();
+    }
+}
