c2f3b1dd0c4c12187ea64f60649bd093568237fc
SangJun-HitHub <sjpark24@myseneca.ca>
2019-02-27 09:53:19 -0500

    commit

--- Generics_Enumeration_Annotaion/AnnotationEx3.java
+++ Generics_Enumeration_Annotaion/AnnotationEx3.java
@@ -1,11 +1,13 @@
 package Generics_Enumeration_Annotaion;
 
+import java.util.ArrayList;
+
 /**
  * Created by bobsang89@gmail.com on 2019-02-27
  * Project: JAVA2
  * Github : http://github.com/SangJun-GitHub
  */
-class NewClass2{
+class NewClass3{
     int newField;
 
     int getNewField(){
@@ -20,11 +22,16 @@ class NewClass2{
         return oldField;
     }
 }
-public class AnnotationEx2 {
+public class AnnotationEx3 {
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
-        NewClass2 newClass = new NewClass2();
+        NewClass3 newClass = new NewClass3();
 
-        newClass.oldField = 10;
+        newClass.oldField =10;
         System.out.println(newClass.getOldField());
+
+        @SuppressWarnings("unchecked")
+        ArrayList<NewClass3> list = new ArrayList();
+        list.add(newClass);
     }
 }
