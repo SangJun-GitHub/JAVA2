b5ce81ef5a4962d78354d63650fcbd073615c27b
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-01 10:24:33 -0500

    commit

---
+++ Thread/ThreadEx4.java
@@ -0,0 +1,21 @@
+package Thread;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-03-01
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class ThreadEx4 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        for(int i = 0; i < 300; i++)
+            System.out.printf("%s", new String("-"));
+        System.out.println();
+        System.out.println("Total required time1 : " + (System.currentTimeMillis() - startTime));
+        for(int i = 0; i < 300; i ++)
+            System.out.printf("%s", new String("|"));
+        System.out.println();
+        System.out.println("Total required time2 : " + (System.currentTimeMillis() - startTime));
+    }
+}
