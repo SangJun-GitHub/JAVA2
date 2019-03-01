1bd1fee2bbdbfc486b42c268132456f4267256eb
SangJun-HitHub <sjpark24@myseneca.ca>
2019-02-28 19:16:34 -0500

    commit

--- Thread/ThreadEx1.java
+++ Thread/ThreadEx1.java
@@ -5,5 +5,27 @@ package Thread;
  * Project: JAVA2
  * Github : http://github.com/SangJun-GitHub
  */
+class ThreadEx1_1 extends Thread{
+    public void run(){
+        for(int i = 0; i < 5; i++)
+            System.out.println(getName());
+    }
+}
+class ThreadEx1_2 implements Runnable{
+    public void run(){
+        for(int i = 0; i < 5; i++)
+            System.out.println(Thread.currentThread().getName());
+    }
+}
+
 public class ThreadEx1 {
+    public static void main(String[] args) {
+        ThreadEx1_1 t1 = new ThreadEx1_1();
+
+        Runnable r = new ThreadEx1_2();
+        Thread t2 = new Thread(r);      //constructor Thread(Runnable target)
+
+        t1.start();
+        t2.start();
+    }
 }
