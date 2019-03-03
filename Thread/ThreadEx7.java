5637d1707e040fcc5bd523a1a4276adbb934baaa
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-03 12:44:53 -0500

    commit

--- Thread/ThreadEx7.java
+++ Thread/ThreadEx7.java
@@ -1,9 +1,28 @@
 package Thread;
 
+import javax.swing.*;
+
 /**
  * Created by bobsang89@gmail.com on 2019-03-02
  * Project: JAVA2
  * Github : http://github.com/SangJun-GitHub
  */
+class ThreadEx7_1 extends Thread{
+    public void run(){
+        for(int i =10; i >0; i--){
+            System.out.println(i);
+            try{
+                sleep(1000);
+            }catch (Exception e){}
+        }
+    }
+}
 public class ThreadEx7 {
+    public static void main(String[] args) {
+        ThreadEx7_1 th1 = new ThreadEx7_1();
+        th1.start();
+        
+        String input = JOptionPane.showInputDialog("Enter text");
+        System.out.println("Input is " + input);
+    }
 }
