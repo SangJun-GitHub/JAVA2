e16fb78f76d56f21036ceda7b085b2f3713f1121
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-14 21:35:00 -0400

    commit

---
+++ Input_Output/IOEx2.java
@@ -0,0 +1,33 @@
+package Input_Output;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-03-14
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class IOEx2 {
+    public static void main(String[] args) {
+        byte[] inSrc = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        byte[] outSrc = null;
+        byte[] temp = new byte[10];
+
+        ByteArrayInputStream input = null;
+        ByteArrayOutputStream output = null;
+
+        input = new ByteArrayInputStream(inSrc);
+        output = new ByteArrayOutputStream();
+
+        input.read(temp, 0, temp.length);
+        output.write(temp,5, 5);
+
+        outSrc = output.toByteArray();
+
+        System.out.println("Input Source  : " + Arrays.toString(inSrc));
+        System.out.println("temp          : " + Arrays.toString(temp));
+        System.out.println("Output Source : " + Arrays.toString(outSrc));
+    }
+}
