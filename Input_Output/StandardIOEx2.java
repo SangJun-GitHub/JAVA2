3e15c43501bd38c5fd5a275c165069d9fc074cf2
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-20 20:56:09 -0400

    commit

---
+++ Input_Output/StandardIOEx2.java
@@ -0,0 +1,13 @@
+package Input_Output;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-03-20
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class StandardIOEx2 {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.err.println("err : Hello World!");
+    }
+}
