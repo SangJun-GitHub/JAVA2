2d85a1a600dc1b89f8ec690e78511aaa3f8cf64f
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-30 19:36:44 -0400

    commit

---
+++ Networking/TcpIpMultichatClient.java
@@ -0,0 +1,19 @@
+package Networking;
+
+/**
+ * Created by bobsang89@gmail.com on 2019-03-30
+ * Project: JAVA2
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class TcpIpMultichatClient {
+    public static void main(String[] args) {
+        if(args.length != 1){
+            System.out.println("USAGE: java TcpIpMultichatClient userName");
+            System.exit(0);
+        }
+
+        try{
+            String serverIp = "192.168.2.237";
+        }
+    }
+}
