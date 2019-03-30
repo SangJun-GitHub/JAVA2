4cef1b44f77e0c20e1254826b767f4705e853091
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-30 19:38:39 -0400

    commit

--- Networking/UdpClient.java
+++ Networking/UdpClient.java
@@ -1,9 +1,17 @@
 package Networking;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.UnknownHostException;
+
 /**
  * Created by bobsang89@gmail.com on 2019-03-30
  * Project: JAVA2
  * Github : http://github.com/SangJun-GitHub
  */
 public class UdpClient {
+    public static void main(String[] args) throws IOException, UnknownHostException {
+        DatagramSocket datagramSocket = new DatagramSocket();
+
+    }
 }
