a3103002111e5121414f0e2019c0a1890d8d03c5
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-31 23:08:42 -0400

    commit

--- Networking/TcpIpMultichatClient.java
+++ Networking/TcpIpMultichatClient.java
@@ -1,5 +1,11 @@
 package Networking;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by bobsang89@gmail.com on 2019-03-30
  * Project: JAVA2
@@ -14,6 +20,45 @@ public class TcpIpMultichatClient {
 
         try{
             String serverIp = "192.168.2.237";
+            Socket socket = new Socket(serverIp, 7777);
+            System.out.println("Connecting to server");
+            Thread sender = new Thread(new ClientSender(socket, args[0]));
+            Thread receiver = new Thread(new ClientReceiver(socket));
+
+            sender.start();
+            receiver.start();
+        }catch(ConnectException ce){
+            ce.printStackTrace();
+        }catch(Exception e){}
+    }
+
+    static class ClientSender extends Thread{
+        Socket socket;
+        DataOutputStream out;
+        String name;
+
+        ClientSender(Socket socket, String name){
+            this.socket = socket;
+            try{
+                out = new DataOutputStream(socket.getOutputStream());
+                this.name = name;
+            }catch(Exception e){}
         }
+
+        public void run(){
+            Scanner scanner = new Scanner(System.in);
+            try{
+                if(out!=null){
+                    out.writeUTF(name);
+                }
+                while(out != null){
+                    out.writeUTF("[" + name + "]" + scanner.nextLine());
+                }
+            }catch(IOException e){}
+        }
+    }
+
+    static class ClientReceiver extends Thread{
+
     }
 }
