8253667f0f0b804b9489ebdaa1231894df019ef4
SangJun-HitHub <sjpark24@myseneca.ca>
2019-03-31 23:26:58 -0400

    commit

--- Networking/UdpClient.java
+++ Networking/UdpClient.java
@@ -1,7 +1,9 @@
 package Networking;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
@@ -10,8 +12,28 @@ import java.net.UnknownHostException;
  * Github : http://github.com/SangJun-GitHub
  */
 public class UdpClient {
-    public static void main(String[] args) throws IOException, UnknownHostException {
+    public void start() throws IOException, UnknownHostException {
         DatagramSocket datagramSocket = new DatagramSocket();
+        InetAddress serverAddress = InetAddress.getByName("192.168.2.237");
 
+        byte[] msg = new byte[100];
+
+        DatagramPacket outPacket = new DatagramPacket(msg, 1, serverAddress, 7777);
+        DatagramPacket inPacket = new DatagramPacket(msg, msg.length);
+
+        datagramSocket.send(outPacket);
+        datagramSocket.receive(inPacket);
+
+        System.out.println("current server time : " + new String(inPacket.getData()));\
+
+        datagramSocket.close();
+    }
+
+    public static void main(String[] args) {
+        try{
+            new UdpClient().start();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
